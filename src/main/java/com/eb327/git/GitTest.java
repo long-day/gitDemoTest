f2f1b0d5ff70353666f603705a0e5e155082da5f
jun <rosyevening@qq.com>
2022-03-27 22:04:52 +0800

    IDEA 提交测试

---
+++ src/main/java/com/eb327/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.eb327.git;
+
+/**
+ * @author 君
+ * @version 1.0
+ * @date 2022/3/27 22:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
