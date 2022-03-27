45a7dd549dd884df1b3bfad7935e32326672c6bd
jun <rosyevening@qq.com>
2022-03-27 22:20:35 +0800

    hot fix 第一次提交

--- src/main/java/com/eb327/git/GitTest.java
+++ src/main/java/com/eb327/git/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git3");
+        System.out.println("hello hotfix4");
+
     }
 }
