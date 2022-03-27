d44a84a161d930a600e5b2a503c0079a599440f8
jun <rosyevening@qq.com>
2022-03-27 22:07:16 +0800

    第二次提交

--- src/main/java/com/eb327/git/GitTest.java
+++ src/main/java/com/eb327/git/GitTest.java
@@ -8,5 +8,6 @@ package com.eb327.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
